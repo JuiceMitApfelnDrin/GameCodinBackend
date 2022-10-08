9e0518c2e0d102e4a58a14638aafacb70f009a57
j4at <fourajbira16@gmail.com>
2022-10-08 22:25:30 +0100

    Implement, GameRoomConfig.as_dict, GameRoomConfig.from_dict

--- GameCodin/game_room/config.py
+++ GameCodin/game_room/config.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
-from dataclasses import asdict, dataclass
+from dataclasses import dataclass
 
 from ..puzzle.puzzle_type import PuzzleType
 from ..submission.language import Language
 # from puzzle.puzzle_difficulty import Difficulty
 from .visibility import GameRoomVisibility
+from GameCodinBackend.GameCodin.game_room import visibility
 
 # TODO: for version 0.2.0:
 # difficulty: Difficulty = Difficulty.RANDOM
@@ -14,14 +15,29 @@ from .visibility import GameRoomVisibility
 @dataclass
 class GameRoomConfig:
     game_mode: PuzzleType
-    languages: list[Language]
+    languages: tuple[Language]
 
     duration_minutes: int = 15
     visibility: GameRoomVisibility = GameRoomVisibility.PUBLIC
 
-    def as_dict(self) -> dict:
-        return asdict(self)
-
     @classmethod
     def from_dict(cls, info: dict) -> GameRoomConfig:
-        raise NotImplementedError
+        game_mode_name = info["game_mode"]
+        visibility_name = info["visibility"]
+        return cls(
+            game_mode = PuzzleType[game_mode_name],
+            languages = tuple(Language.get(lang_name) for lang_name in info["langugaes"]),
+            duration_minutes = info["duration_minutes"],
+            visibility = GameRoomVisibility(visibility_name)
+        )
+
+
+    def as_dict(self) -> dict:
+        return {
+            "game_mode": self.game_mode.name,
+            "languages": tuple(lang.name for lang in self.languages),
+            "duration_minutes": self.duration_minutes,
+            "visiblitity": self.visibility.name
+        }
+
+    
