c3895864a2ad2af30632fbbf07e32c40c2424592
reeveng <govaertreeven@gmail.com>
2022-10-22 21:35:01 +0200

    fix test_queries and test_data

--- test_queries.py
+++ test_queries.py
@@ -10,7 +10,7 @@ from GameCodin.user.user import User
 
 # Puzzles
 print("\nPuzzle by id")
-print(Puzzle.get_by_id(id=ObjectId("6338b06459d723d748ed7fb1")))
+print(Puzzle.get_by_id(puzzle_id=ObjectId("6338b06459d723d748ed7fb1")))
 
 print("\nPuzzles by type")
 print(Puzzle.get_by_type(PuzzleType.FASTEST))
@@ -21,7 +21,7 @@ print(Puzzle.get_by_author(author_id=ObjectId("6333585a0b6e7d94a0c64ce3")))
 
 # Users
 print("\nUser by id")
-print(User.get_by_id(id=ObjectId("6345b74a6708cb008dd170d4")))
+print(User.get_by_id(user_id=ObjectId("6345b74a6708cb008dd170d4")))
 
 print("\nUsers that include X in username")
-print(User.get_by_username("u"))
+print(User.get_by_nickname("u"))
